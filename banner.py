b31b4b2288063fbf4205dabfeb1fe1d932e11947
V <119004237+V2Valerie@users.noreply.github.com>
2023-03-09 00:39:13 +0330

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,24 @@
+import os
+
+col, row= os.get_terminal_size()
+
+
+banner = '''
+   ▌ ▐· ▄▄▄· ▄▄▌  ▄▄▄ .▄▄▄  ▪  ▄▄▄ .
+  ▪█·█▌▐█ ▀█ ██•  ▀▄.▀·▀▄ █·██ ▀▄.▀·
+  ▐█▐█•▄█▀▀█ ██▪  ▐▀▀▪▄▐▀▀▄ ▐█·▐▀▀▪▄
+   ███ ▐█ ▪▐▌▐█▌▐▌▐█▄▄▌▐█•█▌▐█▌▐█▄▄▌
+  . ▀   ▀  ▀ .▀▀▀  ▀▀▀ .▀  ▀▀▀▀ ▀▀▀ 
+     '''
+
+green = "\033[1;32m"
+purple = "\033[1;95m"
+blink = "\033[5m"
+reset = "\033[m"
+
+banner = banner.replace("•", f"{blink}{green}•{reset}")
+banner = banner.replace("▪", f"{blink}{purple}▪{reset}")
+banner = banner.replace("·", f"{blink}·{reset}")
+
+
+print(banner)
